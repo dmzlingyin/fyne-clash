0879bcc12c342eaefc54c8019503e0da9108d640
dmzlingyin <dmzlingyin@163.com>
2022-01-27 17:36:05 +0800

    finish layout of profile

--- README.md
+++ README.md
@@ -7,8 +7,6 @@
 - [x] change proxy
 - [x] start with linux
 - [x] system proxy
-- [ ] config.yaml update
-
-### YAML fields
-1. system-proxy: true or false
-2. auto-start: true or false
+- [ ] profile layout
+- [ ] logs layout
+- [ ] unicode support
--- layout/profiles.go
+++ layout/profiles.go
@@ -1,12 +1,27 @@
 package layout
 
 import (
-	"image/color"
+	"fmt"
 
 	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+)
+
+var (
+	input = widget.NewEntry()
 )
 
 func profilesScreen() fyne.CanvasObject {
-	return canvas.NewRectangle(color.White)
+	input.SetPlaceHolder("please input a url or copy a url here")
+	download := widget.NewButton("Download", configDownload)
+	topPanal := container.NewGridWithColumns(2, input, download)
+
+	content := container.New(layout.NewBorderLayout(topPanal, nil, nil, nil), topPanal)
+	return content
+}
+
+func configDownload() {
+	fmt.Println(input.Text)
 }
