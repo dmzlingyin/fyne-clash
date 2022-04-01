2e232196a83b9c97e799379f67842ff72ee26b78
dmzlingyin <dmzlingyin@163.com>
2022-04-01 22:05:17 +0800

    support chinese font.

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ import (
 
 func main() {
 	app := app.New()
-	myTheme := theme.Theme{}
+	myTheme := theme.MyTheme{}
+	myTheme.SetFonts("./data/font.ttf", "")
 	app.Settings().SetTheme(&myTheme)
 	window := app.NewWindow("clashG")
 
